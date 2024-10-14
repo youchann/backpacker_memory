b0357eb25f60402daf4e1478892ad0ebe96a8063
Yutaro Hayashi <yutaro50@gmail.com>
2024-10-15 08:58:51 +0900

    Replace CSS Reset

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import Map from "./Map";
 import "./App.css";
+import { CssBaseline } from "@mui/material";
 
 const App = () => {
-  return <Map />;
+  return (
+    <>
+      <CssBaseline />
+      <Map />
+    </>
+  );
 };
 
 export default App;
