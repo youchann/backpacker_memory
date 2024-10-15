9b039762e80243c629e485dca40e17b145f02ee8
Yutaro Hayashi <yutaro50@gmail.com>
2024-10-15 14:16:00 +0900

    Update index.tsx

--- frontend/src/Map/index.tsx
+++ frontend/src/Map/index.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useState } from "react";
 import Drawer from "./Drawer";
 import GoogleMap from "./GoogleMap";
+import { Box, Skeleton } from "@mui/material";
+import theme from "../theme";
 
 const Component = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
@@ -11,7 +13,24 @@ const Component = () => {
     <>
       <GoogleMap onClickPin={handleClickPin} />
       <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-        <div>hoge</div>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: theme.spacing(1),
+            width: "100%",
+          }}
+        >
+          {[...Array(5)].map((_, index) => (
+            <Skeleton
+              key={index}
+              variant="rectangular"
+              width={100}
+              height={100}
+              sx={{ flexShrink: 0 }}
+            />
+          ))}
+        </Box>
       </Drawer>
     </>
   );
